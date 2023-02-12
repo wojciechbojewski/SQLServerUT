4c4380e6ac05e84c9b36208d9737bac73f83e0ed
Wojciech Bojewski <wojciech.bojewski@gmail.com>
2023-02-12 15:57:33 +0100

    Add SimpleAdd procedure, with test!

--- DB.Test/SQLServerUT.cs
+++ DB.Test/SQLServerUT.cs
@@ -30,5 +30,33 @@ namespace DB.Test
             }
 
         }
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(-5, 5, 0)]
+        [InlineData(-2, -5, -7)]
+        public void usp_SimpleAdd_IntegerValues_Calculated(int a, int b, int expected)
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=192.168.100.30,1433;Initial Catalog=SQLServerUT;Integrated Security=SSPI;"))
+            {
+                SqlCommand cmd = new SqlCommand("dbo.usp_SimpleAdd", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter rc = cmd.Parameters.Add("@Return Value", SqlDbType.Int);
+                rc.Direction = ParameterDirection.ReturnValue;
+
+                cmd.Parameters.Add("@a", SqlDbType.Int).Value = a;
+                cmd.Parameters.Add("@b", SqlDbType.Int).Value = b;
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();               
+                int result = (int)cmd.Parameters["@Return Value"].Value;
+
+                Assert.Equal(expected, result);
+
+            }
+
+        }
+
     }
 }
